9badb112e736699f72d1ed9718becb34513cf6ce
sharhan-alhassan <sharhanalhassan@gmail.com>
2025-09-14 21:47:26 +0000

    patch: add polls urls and views

--- django-102/polls/urls.py
+++ django-102/polls/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('my-polls/', views.polls_page, name='polls_page'),
+    path('my-polls-1/', views.polls_page_one, name='polls_page_one'),
+    path('my-polls-2/', views.polls_page_two, name='polls_page_two'),
 ]
--- django-102/polls/views.py
+++ django-102/polls/views.py
@@ -3,7 +3,10 @@
 from django.http import HttpResponse
 
 def polls_page(request):
-    return HttpResponse("Hello, Stephen. You're at the polls page.")
+    return HttpResponse("Page 0.")
 
+def polls_page_one(request):
+    return HttpResponse("Page 1.")
 
-
+def polls_page_two(request):
+    return HttpResponse("Page 2.")
